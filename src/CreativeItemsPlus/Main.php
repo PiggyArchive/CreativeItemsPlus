ab2d359c82939f0f3c5dd36914dbcaad7afb48c1
MCPEPIG <mcpepig123@gmail.com>
2016-07-07 18:33:50 -0700

    Added notes to config

--- src/CreativeItemsPlus/Main.php
+++ src/CreativeItemsPlus/Main.php
@@ -9,18 +9,14 @@ use pocketmine\utils\Config;
 class Main extends PluginBase{
     public function onEnable(){
         @mkdir($this->getDataFolder());
-        $this->creativeitems = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
-            "full-customization" => false,
-            "items-added" => array(),
-            "items-removed" => array()
-        ));
-        if($this->creativeitems->get("full-customization")){
+        $this->saveDefaultConfig();
+        if($this->getConfig()->get("full-customization")){
             Item::clearCreativeItems();
         }
-        foreach($this->creativeitems->get("items-removed") as $item => $damagevalue){
+        foreach($this->getConfig()->get("items-removed") as $item => $damagevalue){
             Item::removeCreativeItem(Item::get($item, $damagevalue));   
         }
-        foreach($this->creativeitems->get("items-added") as $item => $damagevalue){
+        foreach($this->getConfig()->get("items-added") as $item => $damagevalue){
             Item::addCreativeItem(Item::get($item, $damagevalue));      
         }
         $this->getLogger()->info("§aEnabled.");
