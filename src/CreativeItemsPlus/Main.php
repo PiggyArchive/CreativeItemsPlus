bae0b331116d83470ffe6d953172f26ba72de330
Mr. Piggy <mcpepig123@gmail.com>
2016-07-13 11:52:49 -0700

    Better Formatting :P

--- src/CreativeItemsPlus/Main.php
+++ src/CreativeItemsPlus/Main.php
@@ -1,23 +1,22 @@
 <?php
-
 namespace CreativeItemsPlus;
 
 use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase{
-    public function onEnable(){
+class Main extends PluginBase {
+    public function onEnable() {
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
-        if($this->getConfig()->get("full-customization")){
+        if($this->getConfig()->get("full-customization")) {
             Item::clearCreativeItems();
         }
-        foreach($this->getConfig()->get("items-removed") as $item => $damagevalue){
-            Item::removeCreativeItem(Item::get($item, $damagevalue));   
+        foreach($this->getConfig()->get("items-removed") as $item => $damagevalue) {
+            Item::removeCreativeItem(Item::get($item, $damagevalue));
         }
-        foreach($this->getConfig()->get("items-added") as $item => $damagevalue){
-            Item::addCreativeItem(Item::get($item, $damagevalue));      
+        foreach($this->getConfig()->get("items-added") as $item => $damagevalue) {
+            Item::addCreativeItem(Item::get($item, $damagevalue));
         }
         $this->getLogger()->info("§aEnabled.");
     }
